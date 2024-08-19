cf51a61c5cebf23d276e8194d6a777685729fd0b
Laurenz Bogen <laurenzbogen@pm.me>
2024-08-19 16:26:48 +0200

    resp draft

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,21 +11,29 @@ import { Instagram } from "lucide-vue-next";
 
 
 <template>
-  <header class="relative flex flex-col lg:flex-col-reverse">
-    <div class="flex justify-center lg:hidden">
+  <header class="relative flex flex-col lg:flex-col">
+    <div class="flex justify-center lg:flex">
       <img src="/logo.png" alt="logo" class="h-36 md:h-56 object-contain" />
     </div>
-    <div class="relative">
-      <ClImage class="w-full object-cover aspect-[1.6] xl:aspect-[1.8]" id="Alou/xrvb2c83zcpfxhm6nwlj" pid="hero" />
-      <div class="absolute bottom-0 left-1/2 -translate-x-1/2 flex justify-between w-full items-end lg:items-center max-w-screen-3xl">
+    <nav class="lg:block hidden items-center p-2 w-full">
+      <div class="max-w-xl flex justify-around m-auto gap-8">
+        <Link class="" variant="link" href="#quartet">Quartet</Link>
+        <Link class="" variant="link" href="#termine">Termine</Link>
+        <Link class="" variant="link" href="#media">Media</Link>
+      </div>
+    </nav>
+    <div class="relative max-w-[120vh] m-auto">
+      <ClImage class="w-full object-cover aspect-[1.6] md:aspect-[1.8]" id="Alou/xrvb2c83zcpfxhm6nwlj" pid="hero" />
+      <div class="absolute bottom-0 left-1/2 -translate-x-1/2 flex justify-between w-full items-end max-w-screen-3xl">
+
 
         <!-- <img :src="url" class="w-full object-cover aspect-[1.6] xl:aspect-[1.8]" id="Alou/xrvb2c83zcpfxhm6nwlj" /> -->
 
-        <div class="opacity-0 lg:opacity-100">
+        <div class="opacity-0 lg:opacity-0">
           <img src="/logo.png" alt="logo" class="invert h-[20vw] object-contain" />
         </div>
 
-        <div class="m-6 md:m-16 lg:pr-32 2xl:pr-56 gap-4 flex">
+        <div class="m-6 gap-4 flex">
           <ContactWrapper client:only>
             <!-- Standard Link Styling -->
             <Button class="text-white underline hover:text-blue-800" size="lg" variant="outline">
@@ -40,7 +48,7 @@ import { Instagram } from "lucide-vue-next";
     </div>
 
 
-    <nav class="lg:absolute lg:top-0 bg-background1 lg:bg-none bg-cover items-center p-2 w-full">
+    <nav class="lg:hidden bg-background1 bg-cover items-center p-2 w-full">
       <div class="max-w-xl flex justify-around m-auto gap-8">
         <Link class="" variant="link" href="#quartet">Quartet</Link>
         <Link class="" variant="link" href="#termine">Termine</Link>
